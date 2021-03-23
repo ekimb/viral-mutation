8731c4fdb59d5449ba8844411130d70937eadb66
Barış Ekim <ekimb@mit.edu>
2021-03-23 07:45:42 -0400

    uniprot

---
+++ bin/uniprot.py
@@ -0,0 +1,189 @@
+from mutation import *
+
+np.random.seed(1)
+random.seed(1)
+
+def parse_args():
+    import argparse
+    parser = argparse.ArgumentParser(description='Coronavirus sequence analysis')
+    parser.add_argument('model_name', type=str,
+                        help='Type of language model (e.g., hmm, lstm)')
+    parser.add_argument('--namespace', type=str, default='cov',
+                        help='Model namespace')
+    parser.add_argument('--dim', type=int, default=512,
+                        help='Embedding dimension')
+    parser.add_argument('--batch-size', type=int, default=500,
+                        help='Training minibatch size')
+    parser.add_argument('--n-epochs', type=int, default=11,
+                        help='Number of training epochs')
+    parser.add_argument('--seed', type=int, default=1,
+                        help='Random seed')
+    parser.add_argument('--checkpoint', type=str, default=None,
+                        help='Model checkpoint')
+    parser.add_argument('--train', action='store_true',
+                        help='Train model')
+    parser.add_argument('--train-split', action='store_true',
+                        help='Train model on portion of data')
+    parser.add_argument('--test', action='store_true',
+                        help='Test model')
+    parser.add_argument('--embed', action='store_true',
+                        help='Analyze embeddings')
+    parser.add_argument('--semantics', action='store_true',
+                        help='Analyze mutational semantic change')
+    args = parser.parse_args()
+    return args
+
+def parse_uniprot(entry):
+    fields = entry.split(' ')
+    species_raw = [x for x in fields if "OS=" in x][0]
+    species_idx = [i for i in range(len(fields)) if fields[i] == species_raw][0]
+    protein = " ".join(fields[1:species_idx])
+    print(protein)
+    species = species_raw.split("=")[1]
+    print(species)
+    meta = {
+        'species': species,
+        'protein': protein
+    }
+    return meta
+
+def process(fnames):
+    seqs = {}
+    for fname in fnames:
+        for record in SeqIO.parse(fname, 'fasta'):
+            if str(record.seq).count('X') > 0:
+                continue
+            if record.seq not in seqs:
+                seqs[record.seq] = []
+            meta = parse_uniprot(record.description)
+            meta['accession'] = record.description
+            if meta['protein'] == 'Receptor protein-tyrosine kinase' or meta['protein'] == 'Protein-tyrosine kinase receptor UFO':
+                seqs[record.seq].append(meta)
+
+    with open('uniprot_all.fa', 'w') as of:
+        for seq in seqs:
+            metas = seqs[seq]
+            for meta in metas:
+                of.write('>{}\n'.format(meta['accession']))
+                of.write('{}\n'.format(str(seq)))
+
+    return seqs
+
+def split_seqs(seqs, split_method='random'):
+    train_seqs, test_seqs = {}, {}
+
+    tprint('Splitting seqs...')
+    for idx, seq in enumerate(seqs):
+        if idx % 10 < 2:
+            test_seqs[seq] = seqs[seq]
+        else:
+            train_seqs[seq] = seqs[seq]
+    tprint('{} train seqs, {} test seqs.'
+           .format(len(train_seqs), len(test_seqs)))
+
+    return train_seqs, test_seqs
+
+def setup(args):
+    fnames = ['uniprot-gene_axl.fasta']
+
+    seqs = process(fnames)
+
+    seq_len = max([ len(seq) for seq in seqs ]) + 2
+    vocab_size = len(AAs) + 2
+
+    model = get_model(args, seq_len, vocab_size,
+                      inference_batch_size=1200)
+
+    return model, seqs
+
+def interpret_clusters(adata):
+    clusters = sorted(set(adata.obs['louvain']))
+    for cluster in clusters:
+        tprint('Cluster {}'.format(cluster))
+        adata_cluster = adata[adata.obs['louvain'] == cluster]
+        for var in [ 'species', 'protein']:
+            tprint('\t{}:'.format(var))
+            counts = Counter(adata_cluster.obs[var])
+            for val, count in counts.most_common():
+                tprint('\t\t{}: {}'.format(val, count))
+        tprint('')
+
+def plot_umap(adata, categories, namespace='uniprot'):
+    for category in categories:
+        sc.pl.umap(adata, color=category,
+                   save='_{}_{}.png'.format(namespace, category))
+
+def analyze_embedding(args, model, seqs, vocabulary):
+    seqs = embed_seqs(args, model, seqs, vocabulary, use_cache=True)
+
+    X, obs = [], {}
+    obs['n_seq'] = []
+    obs['seq'] = []
+    for seq in seqs:
+        meta = seqs[seq][0]
+        X.append(meta['embedding'].mean(0))
+        for key in meta:
+            if key == 'embedding':
+                continue
+            if key not in obs:
+                obs[key] = []
+            obs[key].append(Counter([
+                meta[key] for meta in seqs[seq]
+            ]).most_common(1)[0][0])
+        obs['n_seq'].append(len(seqs[seq]))
+        obs['seq'].append(str(seq))
+    X = np.array(X)
+
+    adata = AnnData(X)
+    for key in obs:
+        adata.obs[key] = obs[key]
+
+    sc.pp.neighbors(adata, n_neighbors=20, use_rep='X')
+    sc.tl.louvain(adata, resolution=1.)
+    sc.tl.umap(adata, min_dist=1.)
+
+    sc.set_figure_params(dpi_save=500)
+    plot_umap(adata, [ 'species', 'protein', 'louvain' ])
+
+    interpret_clusters(adata)
+
+    adata_uniprot = adata[(adata.obs['louvain'] == '0') |
+                       (adata.obs['louvain'] == '2')]
+    plot_umap(adata_uniprot, [ 'species', 'protein' ],
+              namespace='uniprot')
+
+if __name__ == '__main__':
+    args = parse_args()
+
+    AAs = [
+        'A', 'R', 'N', 'D', 'C', 'Q', 'E', 'G', 'H',
+        'I', 'L', 'K', 'M', 'F', 'P', 'S', 'T', 'W',
+        'Y', 'V', 'X', 'Z', 'J', 'U', 'B',
+    ]
+    vocabulary = { aa: idx + 1 for idx, aa in enumerate(sorted(AAs)) }
+
+    model, seqs = setup(args)
+
+    if 'esm' in args.model_name:
+        args.checkpoint = args.model_name
+    elif args.checkpoint is not None:
+        model.model_.load_weights(args.checkpoint)
+        tprint('Model summary:')
+        tprint(model.model_.summary())
+
+    if args.train:
+        batch_train(args, model, seqs, vocabulary, batch_size=1000)
+
+    if args.train_split or args.test:
+        train_test(args, model, seqs, vocabulary, split_seqs)
+
+    if args.embed:
+        if args.checkpoint is None and not args.train:
+            raise ValueError('Model must be trained or loaded '
+                             'from checkpoint.')
+        no_embed = { 'hmm' }
+        if args.model_name in no_embed:
+            raise ValueError('Embeddings not available for models: {}'
+                             .format(', '.join(no_embed)))
+        analyze_embedding(args, model, seqs, vocabulary)
+    
